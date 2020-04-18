b656eaf4331f18fcd6b15f0d90260b2be2a6c534
John Taggart <taggartj1985@gmail.com>
2020-04-18 10:55:03 +0100

    more styling added

--- js/app.js
+++ js/app.js
@@ -1,75 +1,75 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const newItemform = document.querySelector('#new-item-form');
-//   newItemform.addEventListener('submit', handleNewItemFormSubmit);
-//
-//   const deleteAllButton = document.querySelector('#delete-all');
-//   deleteAllButton.addEventListener('click', handleDeleteAllClick);
-// })
-//
-// const handleNewItemFormSubmit = function (event) {
-//   event.preventDefault();
-//
-//   const albumListItem = createAlbumListItem(event.target);
-//   const albumList = document.querySelector('#album-list');
-//   albumList.appendChild(albumListItem);
-//
-//   event.target.reset();
-// }
-//
-// const createAlbumListItem = function (form) {
-//   const albumListItem = document.createElement('li');
-//   albumListItem.classList.add('album-list-item');
-//
-//   const artist = document.createElement('h3');
-//   artist.textContent = form.artist.value;
-//   albumListItem.appendChild(artist);
-//
-//   const album = document.createElement('h3');
-//   album.textContent = form.album.value;
-//   albumListItem.appendChild(album);
-//
-//   const genre = document.createElement('p');
-//   genre.textContent = form.genre.value;
-//   albumListItem.appendChild(genre);
-//
-//   const review = document.createElement('p');
-//   review.textContent = form.review.value;
-//   albumListItem.appendChild(review);
-//
-//   return albumListItem;
-// }
-//
-//
-// const handleDeleteAllClick = function (event) {
-//   const albumList = document.querySelector('#album-list');
-//   albumList.innerHTML = '';
-// }
-
-document.addEventListener('DOMContentLoaded', () =>{
-
+document.addEventListener('DOMContentLoaded', () => {
   const newItemform = document.querySelector('#new-item-form');
   newItemform.addEventListener('submit', handleNewItemFormSubmit);
 
   const deleteAllButton = document.querySelector('#delete-all');
   deleteAllButton.addEventListener('click', handleDeleteAllClick);
-
 })
 
 const handleNewItemFormSubmit = function (event) {
   event.preventDefault();
+
+  const albumListItem = createAlbumListItem(event.target);
+  const albumList = document.querySelector('#album-list');
+  albumList.appendChild(albumListItem);
+
+  event.target.reset();
+}
+
+const createAlbumListItem = function (form) {
   const albumListItem = document.createElement('li');
   albumListItem.classList.add('album-list-item');
-  albumListItem.innerHTML = `
-  <h3>${event.target.artist.value}</h3>
-  <h3>${event.target.album.value}</h3>
-   <p>${event.target.genre.value}</p>
-   <p>${event.target.review.value}</p>`
-  const list = document.querySelector('#album-list');
-  list.appendChild(albumListItem);
-  document.getElementById('#new-item-form').reset();
+
+  const artist = document.createElement('h3');
+  artist.textContent = form.artist.value;
+  albumListItem.appendChild(artist);
+
+  const album = document.createElement('h3');
+  album.textContent = form.album.value;
+  albumListItem.appendChild(album);
+
+  const genre = document.createElement('p');
+  genre.textContent = form.genre.value;
+  albumListItem.appendChild(genre);
+
+  const review = document.createElement('p');
+  review.textContent = form.review.value;
+  albumListItem.appendChild(review);
+
+  return albumListItem;
 }
+
 
 const handleDeleteAllClick = function (event) {
   const albumList = document.querySelector('#album-list');
   albumList.innerHTML = '';
 }
+
+// document.addEventListener('DOMContentLoaded', () =>{
+//
+//   const newItemform = document.querySelector('#new-item-form');
+//   newItemform.addEventListener('submit', handleNewItemFormSubmit);
+//
+//   const deleteAllButton = document.querySelector('#delete-all');
+//   deleteAllButton.addEventListener('click', handleDeleteAllClick);
+//
+// })
+//
+// const handleNewItemFormSubmit = function (event) {
+//   event.preventDefault();
+//   const albumListItem = document.createElement('li');
+//   albumListItem.classList.add('album-list-item');
+//   albumListItem.innerHTML = `
+//   <h3>${event.target.artist.value}</h3>
+//   <h4>${event.target.album.value}</h4>
+//    <p>${event.target.genre.value}</p>
+//    <p>${event.target.review.value}</p>`
+//   const list = document.querySelector('#album-list');
+//   list.appendChild(albumListItem);
+//   document.getElementById('#new-item-form').reset();
+// }
+//
+// const handleDeleteAllClick = function (event) {
+//   const albumList = document.querySelector('#album-list');
+//   albumList.innerHTML = '';
+// }
